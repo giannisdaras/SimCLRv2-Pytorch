dbda09b4d520a10501b01acede33989c238a44da
Giannis Daras <daras.giannhs@gmail.com>
2020-10-25 17:47:12 -0500

    Read mappings and labels from correct source

--- dataloader.py
+++ dataloader.py
@@ -6,20 +6,28 @@ import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 class ImageNetEval(Dataset):
-    def __init__(self, images_path, labels_path):
+    def __init__(self, images_path, labels_path, mappings_path=None):
         self.images_path = images_path
-        self.labels = np.load(labels_path)
+        self.labels = []
+        with open(labels_path, 'r') as f:
+            for line in f.readlines():
+                self.labels.append(int(line.strip('\n')))
+        self.labels = np.array(self.labels)
         self.transform = transforms.Compose([transforms.Resize(256),
                                              transforms.CenterCrop(224),
                                              transforms.ToTensor()])
+        self.mappings = {}
+        if mappings_path is not None:
+            with open(mappings_path, 'r') as f:
+                lines = f.readlines()
+                for line in lines:
+                    number, desc = line.strip('\n').split(' ')[1:]
+                    self.mappings[int(number) - 1] = desc
 
     def __getitem__(self, item):
         img = Image.open(os.path.join(self.images_path, f'ILSVRC2012_val_{item + 1:08d}.JPEG')).convert('RGB')
-        return self.transform(img), int(self.labels[item]) - 1
+        return self.transform(img), self.labels[item]
 
     def __len__(self):
         return len(self.labels)
-
-
