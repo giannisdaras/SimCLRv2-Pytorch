09d44a616a74bf67cc2a9e2c3ae4bff663ba6456
Giannis Daras <daras.giannhs@gmail.com>
2020-10-24 22:06:44 -0500

    Load labels from numpy

--- dataloader.py
+++ dataloader.py
@@ -1,17 +1,18 @@
 import torch
 from torch.utils.data import Dataset
 import os
-import glob
 from PIL import Image
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 class ImageNetEval(Dataset):
     def __init__(self, images_path, labels_path, mappings_path):
         # two fold mappings: image_name => image_label, image_label => image_desc
         self.mapping = self.get_mappings(mappings_path)
         self.images_path = images_path
-        self.labels = self.get_labels(labels_path)
+        self.labels = np.load(labels_path)
         self.transform = transforms.Compose([transforms.Resize(256),
                                              transforms.CenterCrop(224),
                                              transforms.ToTensor()])
@@ -22,19 +23,6 @@ class ImageNetEval(Dataset):
 
     def __len__(self):
         return len(self.labels)
-
-    def get_labels(self, labels_path):
-        val_path = labels_path + 'val/'
-        labels = []
-        files = glob.glob(val_path + '/*')
-        for file in sorted(files):
-            with open(file, 'r') as xml_file:
-                xml_file_lines = xml_file.readlines()
-                for line in xml_file_lines:
-                    if '<name>' in line:
-                        labels.append(self.mapping[line.split('>')[1].split('<')[0]])
-                        break
-        return labels
 
     def get_mappings(self, mappings_path):
         mapping = {}
@@ -49,5 +37,5 @@ class ImageNetEval(Dataset):
 
 if __name__ == '__main__':
     ds = ImageNetEval('/Users/giannis/datasets/ILSVRC2012_img_val',
-                  '/Users/giannis/datasets/Annotations/CLS-LOC/',
-                  '/Users/giannis/datasets/mappings.txt')
+                      '/Users/giannis/datasets/labels.npy',
+                      '/Users/giannis/datasets/mappings.txt')
