Fix parsing script and accuracy calculation

verify.py
@@ -1,3 +1,4 @@
+import numpy as np
 import os
 import argparse
 from collections import Counter
@@ -25,11 +26,9 @@ def accuracy(output, target, topk=(1,)):
 
 @torch.no_grad()
 def run(args):
-    dataset = ImageNetEval('/Users/giannis/datasets/ILSVRC2012_img_val',
-                           '/Users/giannis/datasets/Annotations/CLS-LOC/',
-                           '/Users/giannis/datasets/mappings.txt')
-
-    data_loader = DataLoader(dataset, batch_size=64, shuffle=False, pin_memory=True, num_workers=8)
+    dataset = ImageNetEval(args.images_path, args.labels_path)
+    data_loader = DataLoader(dataset, batch_size=args.batch_size, 
+                             shuffle=False, pin_memory=True, num_workers=8)
     model, _ = get_resnet(*name_to_params(args.pth_path))
     model.load_state_dict(torch.load(args.pth_path)['resnet'])
     model = model.to(args.device).eval()
@@ -38,22 +37,27 @@ def run(args):
     for images, labels in tqdm(data_loader):
         _, pred = model(images.to(args.device), apply_fc=True).topk(1, dim=1)
         preds.append(pred.squeeze(1).cpu())
-        target.append(labels)
+        target += labels
     p = torch.cat(preds).numpy()
-    t = torch.cat(target).numpy()
-
+    t = np.array(target, dtype=np.int32)
     all_counters = [Counter() for i in range(1000)]
-    for i in range(len(dataloader)):
+    for i in range(len(dataset)):
         all_counters[t[i]][p[i]] += 1
     total_correct = 0
     for i in range(1000):
         total_correct += all_counters[i].most_common(1)[0][1]
-    print(f'Accuracy: {total_correct / len(dataloader) * 100}')
+    print(f'Accuracy: {total_correct / len(dataset) * 100}')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='SimCLR verifier')
-    parser.add_argument('pth_path', type=str, help='path of the input checkpoint file')
+    parser.add_argument('pth_path', type=str, 
+                        help='path of the input checkpoint file')
     parser.add_argument('--device', type=str, default='cuda')
+    parser.add_argument('--images_path', type=str, 
+                        default='~/datasets/ILSVRC2012_img_val')
+    parser.add_argument('--labels_path', type=str, 
+                        default='~/datasets/labels.npy')
+    parser.add_argument('--batch_size', type=int, default=128)
     args = parser.parse_args()
     run(args)