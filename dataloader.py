3f95a6098db546c5f90146769f987fb8bfe6b439
Giannis Daras <daras.giannhs@gmail.com>
2020-10-25 14:40:26 -0500

    Remove mappings

--- dataloader.py
+++ dataloader.py
@@ -8,9 +8,7 @@ import numpy as np
 
 
 class ImageNetEval(Dataset):
-    def __init__(self, images_path, labels_path, mappings_path):
-        # two fold mappings: image_name => image_label, image_label => image_desc
-        self.mapping = self.get_mappings(mappings_path)
+    def __init__(self, images_path, labels_path):
         self.images_path = images_path
         self.labels = np.load(labels_path)
         self.transform = transforms.Compose([transforms.Resize(256),
@@ -19,23 +17,9 @@ class ImageNetEval(Dataset):
 
     def __getitem__(self, item):
         img = Image.open(os.path.join(self.images_path, f'ILSVRC2012_val_{item + 1:08d}.JPEG')).convert('RGB')
-        return self.transform(img), self.labels[item]
+        return self.transform(img), int(self.labels[item]) - 1
 
     def __len__(self):
         return len(self.labels)
 
-    def get_mappings(self, mappings_path):
-        mapping = {}
-        with open(mappings_path, 'r') as f:
-            lines = f.readlines()
-            for line in lines:
-                image_name, image_label, image_desc = line.split(' ')
-                mapping[image_name] = image_label
-                mapping[image_label]  = image_desc.strip('\n')
-        return mapping
 
-
-if __name__ == '__main__':
-    ds = ImageNetEval('/Users/giannis/datasets/ILSVRC2012_img_val',
-                      '/Users/giannis/datasets/labels.npy',
-                      '/Users/giannis/datasets/mappings.txt')
