eaa02f94ed46b89c3e4f3c57d67b0765073a4a11
Giannis Daras <daras.giannhs@gmail.com>
2020-10-24 21:59:40 -0500

    Use custom dataloder & flexible dataset size

--- verify.py
+++ verify.py
@@ -1,7 +1,6 @@
 import os
 import argparse
 from collections import Counter
-
 import torch
 from PIL import Image
 from tqdm import tqdm
@@ -25,32 +24,36 @@ def accuracy(output, target, topk=(1,)):
 
 
 @torch.no_grad()
-def run(pth_path):
-    device = 'cuda'
-    dataset = ImagenetValidationDataset('./val/')
+def run(args):
+    dataset = ImageNetEval('/Users/giannis/datasets/ILSVRC2012_img_val',
+                           '/Users/giannis/datasets/Annotations/CLS-LOC/',
+                           '/Users/giannis/datasets/mappings.txt')
+
     data_loader = DataLoader(dataset, batch_size=64, shuffle=False, pin_memory=True, num_workers=8)
-    model, _ = get_resnet(*name_to_params(pth_path))
-    model.load_state_dict(torch.load(pth_path)['resnet'])
-    model = model.to(device).eval()
+    model, _ = get_resnet(*name_to_params(args.pth_path))
+    model.load_state_dict(torch.load(args.pth_path)['resnet'])
+    model = model.to(args.device).eval()
     preds = []
     target = []
     for images, labels in tqdm(data_loader):
-        _, pred = model(images.to(device), apply_fc=True).topk(1, dim=1)
+        _, pred = model(images.to(args.device), apply_fc=True).topk(1, dim=1)
         preds.append(pred.squeeze(1).cpu())
         target.append(labels)
     p = torch.cat(preds).numpy()
     t = torch.cat(target).numpy()
+
     all_counters = [Counter() for i in range(1000)]
-    for i in range(50000):
+    for i in range(len(dataloader)):
         all_counters[t[i]][p[i]] += 1
     total_correct = 0
     for i in range(1000):
         total_correct += all_counters[i].most_common(1)[0][1]
-    print(f'ACC: {total_correct / 50000 * 100}')
+    print(f'Accuracy: {total_correct / len(dataloader) * 100}')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='SimCLR verifier')
     parser.add_argument('pth_path', type=str, help='path of the input checkpoint file')
+    parser.add_argument('--device', type=str, default='cuda')
     args = parser.parse_args()
-    run(args.pth_path)
+    run(args)
